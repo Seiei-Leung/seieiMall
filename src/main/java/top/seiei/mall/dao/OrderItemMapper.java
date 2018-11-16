6ef7067550c91888b3704bf8628d29d5d609103a
AB786883603 <786883603@qq.com>
2018-11-17 00:27:49 +0800

    Mybatis generator 初始化 dao, bean, mapper

---
+++ src/main/java/top/seiei/mall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package top.seiei.mall.dao;
+
+import top.seiei.mall.bean.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/top/seiei/mall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package top.seiei.mall.dao;
+
+import top.seiei.mall.bean.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
